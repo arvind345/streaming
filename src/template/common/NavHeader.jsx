96375215bb1296a039d91df6f9a2c47061d29c5e
91shreya <shreya@91mobiles.com>
2018-07-20 15:58:33 +0530

    streaming

---
+++ src/template/common/NavHeader.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class NavHeader extends Component {
+    render() {
+        return (
+            <div>
+               <h1>navigation</h1>
+            </div>
+        );
+    }
+}
+
+export default NavHeader;
