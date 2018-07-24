1e3846f9f65325f87e077b7d1d6372565eba8ad2
91shreya <shreya@91mobiles.com>
2018-07-24 13:10:48 +0530

    straeming css

--- src/app/home/Home.jsx
+++ src/app/home/Home.jsx
@@ -6,14 +6,14 @@ class Home extends Component {
                 <div className="home-banner">
                     <img className="w-100" src="https://www.91-cdn.com/upcomingg-img/banners/banner_images/000/000/006/original/Upcoming-movies-V1.jpg?1530705594" />
                 </div>
-                <div className="homepage">
-                    <div className="row no-gutters">
-                        <div className="col-md-12 mt-3 mb-3">
+                <div className="container homepage">
+
+
                             <div className="home-tab"></div>
                             <div className="">
                                 <div className="card-wrapper">
-                                    <div className="card-header m-2">Geners</div>
-                                    <div className="card-slider ml-2">
+                                    <div className="card-header mt-2">Geners</div>
+                                    <div className="card-slider">
                                         <div className="card-div sm-card mr-2">
                                             <a href="#" className="card-sm-img img-top">
                                                 <img src="https://www.91-cdn.com/upcomingg-img/tr:h-215,w-375,q-100,fo-auto/images/images/000/007/461/original/1.jpg?1531212727"/>
@@ -46,8 +46,8 @@ class Home extends Component {
                             </div>
                             <div className="">
                                 <div className="card-wrapper">
-                                    <div className="card-header m-2">Popular TV Shows</div>
-                                    <div className="card-slider ml-2">
+                                    <div className="card-header mt-2">Popular TV Shows</div>
+                                    <div className="card-slider">
                                         <div className="card-div lg-card mr-3">
                                             <a href="#" className="card-lg-img img-top">
                                                 <img src="//in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/dhadak-et00065160-14-11-2017-11-12-35.jpg"/>
@@ -78,8 +78,13 @@ class Home extends Component {
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
+                            {/*channel list start */}
+                            <div className="channel-wrapper">
+                                <div></div>
+                            </div>
+                            {/*channel list end*/}
+
+
                 </div>
             </div>
         );
