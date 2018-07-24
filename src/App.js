9bfb89b7e951d09fff237243e030da28c81d29e8
arvindsharna <arvind@91mobiles.com>
2018-07-24 15:34:54 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import NavHeader from './app/common/NavHeader';
 import Footer from './app/common/Footer';
 import Home from './app/home/Home';
+import Details from './app/detailPage/detail';
+
 //import './App.scss';
-import './assets/css/common.scss'
+import './assets/css/common.scss';
+import {BrowserRouter,  Route, Switch } from 'react-router-dom';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <NavHeader/>
-          <Home/>
-          <Footer/>
-      </div>
+        <BrowserRouter>
+            <div>
+                <NavHeader/>
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/detail'  component={Details} />
+                </Switch>
+            </div>
+        </BrowserRouter>
     );
   }
 }
--- src/app/common/NavHeader.jsx
+++ src/app/common/NavHeader.jsx
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
+import { NavLink } from "react-router-dom";
 
 
 class NavHeader extends Component {
     render() {
         return (
             <div>
-               <h1>navigation</h1>
+
+                <h1>{}</h1>
+
+                <NavLink to="/" className="button">Home</NavLink> |
+                <NavLink to="/detail" className="button"> Detail Page</NavLink>
+
             </div>
         );
     }
