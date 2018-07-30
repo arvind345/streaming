9eed186fb278ed43a37bff7e4a214e246f978098
91shreya <shreya@91mobiles.com>
2018-07-30 18:23:42 +0530

    home page

--- src/app/home/Home.jsx
+++ src/app/home/Home.jsx
@@ -5,84 +5,221 @@ class Home extends Component {
             <div>
                 <div className="home-banner">
                     <img className="w-100" src="https://www.91-cdn.com/upcomingg-img/banners/banner_images/000/000/006/original/Upcoming-movies-V1.jpg?1530705594" />
-                    <div className="home-search d-sm-none">
+                    <div className="home-search">
                         <div className="home-search-title">THE STREAMING SEARCH ENGINE</div>
                         <div className="home-search-input"><i className=""></i> Search </div>
                     </div>
                 </div>
                 <div className="container homepage">
                             <div className="home-tab"></div>
-                            <div className="">
+                            <div className="no-m-card-component card-component padd">
                                 <div className="card-wrapper">
                                     <div className="card-header mt-2">Geners</div>
                                     <div className="card-slider">
                                         <div className="card-div sm-card mr-2">
                                             <a href="#" className="card-sm-img img-top">
-                                                <img src="http://via.placeholder.com/150x78"/>
+                                                <img src="http://via.placeholder.com/210x105"/>
                                             </a>
                                             <div className="card-bottom">
                                                 <div className="card-title">Drama</div>
                                                 <div className="card-subtitle">196 Shows, 1161 Movies</div>
+                                                <div className="btn btn-outline-coral d-desktop-inblock">View Details</div>
                                             </div>
                                         </div>
                                         <div className="card-div sm-card mr-2">
                                             <a href="#" className="card-sm-img img-top">
-                                                <img src="http://via.placeholder.com/150x78"/>
+                                                <img src="http://via.placeholder.com/210x105"/>
                                             </a>
                                             <div className="card-bottom">
                                                 <div className="card-title">Drama</div>
                                                 <div className="card-subtitle">196 Shows, 1161 Movies</div>
+                                                <div className="btn btn-outline-coral d-desktop-inblock">View Details</div>
                                             </div>
                                         </div>
                                         <div className="card-div sm-card mr-2">
                                             <a href="#" className="card-sm-img img-top">
-                                                <img src="http://via.placeholder.com/150x78"/>
+                                                <img src="http://via.placeholder.com/210x105"/>
                                             </a>
                                             <div className="card-bottom">
                                                 <div className="card-title">Drama</div>
                                                 <div className="card-subtitle">196 Shows, 1161 Movies</div>
+                                                <div className="btn btn-outline-coral d-desktop-inblock">View Details</div>
+                                            </div>
+                                        </div>
+                                        <div className="card-div sm-card mr-2">
+                                            <a href="#" className="card-sm-img img-top">
+                                                <img src="http://via.placeholder.com/210x105"/>
+                                            </a>
+                                            <div className="card-bottom">
+                                                <div className="card-title">Drama</div>
+                                                <div className="card-subtitle">196 Shows, 1161 Movies</div>
+                                                <div className="btn btn-outline-coral d-desktop-inblock">View Details</div>
+                                            </div>
+                                        </div>
+                                        <div className="card-div sm-card mr-2">
+                                            <a href="#" className="card-sm-img img-top">
+                                                <img src="http://via.placeholder.com/210x105"/>
+                                            </a>
+                                            <div className="card-bottom">
+                                                <div className="card-title">Drama</div>
+                                                <div className="card-subtitle">196 Shows, 1161 Movies</div>
+                                                <div className="btn btn-outline-coral d-desktop-inblock">View Details</div>
+                                            </div>
+                                        </div>
+                                        <div className="card-div sm-card mr-2">
+                                            <a href="#" className="card-sm-img img-top">
+                                                <img src="http://via.placeholder.com/210x105"/>
+                                            </a>
+                                            <div className="card-bottom">
+                                                <div className="card-title">Drama</div>
+                                                <div className="card-subtitle">196 Shows, 1161 Movies</div>
+                                                <div className="btn btn-outline-coral d-desktop-inblock">View Details</div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className="">
+                            <div className="no-m-card-component card-component padd">
                                 <div className="card-wrapper">
                                     <div className="card-header mt-2">Popular TV Shows</div>
                                     <div className="card-slider">
                                         <div className="card-div lg-card mr-3">
                                             <a href="#" className="card-lg-img img-top">
-                                                <img src="http://via.placeholder.com/210x210"/>
+                                                <img src="http://via.placeholder.com/280x250"/>
                                             </a>
                                             <div className="card-bottom">
                                                 <div className="card-title">Drama</div>
                                                 <div className="card-subtitle">196 Shows, 1161 Movies</div>
+                                                <div className="btn btn-outline-coral d-desktop-inblock">Watch Now</div>
                                             </div>
                                         </div>
                                         <div className="card-div lg-card mr-3">
                                             <a href="#" className="card-lg-img img-top">
-                                                <img src="http://via.placeholder.com/210x210"/>
+                                                <img src="http://via.placeholder.com/280x250"/>
                                             </a>
                                             <div className="card-bottom">
                                                 <div className="card-title">Drama</div>
                                                 <div className="card-subtitle">196 Shows, 1161 Movies</div>
+                                                <div className="btn btn-outline-coral d-desktop-inblock">Watch Now</div>
                                             </div>
                                         </div>
                                         <div className="card-div lg-card mr-3">
                                             <a href="#" className="card-lg-img img-top">
-                                                <img src="http://via.placeholder.com/210x210"/>
+                                                <img src="http://via.placeholder.com/280x250"/>
                                             </a>
                                             <div className="card-bottom">
                                                 <div className="card-title">Drama</div>
                                                 <div className="card-subtitle">196 Shows, 1161 Movies</div>
+                                                <div className="btn btn-outline-coral d-desktop-inblock">Watch Now</div>
+                                            </div>
+                                        </div>
+                                        <div className="card-div lg-card mr-3">
+                                            <a href="#" className="card-lg-img img-top">
+                                                <img src="http://via.placeholder.com/280x250"/>
+                                            </a>
+                                            <div className="card-bottom">
+                                                <div className="card-title">Drama</div>
+                                                <div className="card-subtitle">196 Shows, 1161 Movies</div>
+                                                <div className="btn btn-outline-coral d-desktop-inblock">Watch Now</div>
+                                            </div>
+                                        </div>
+                                        <div className="card-div lg-card mr-3">
+                                            <a href="#" className="card-lg-img img-top">
+                                                <img src="http://via.placeholder.com/280x250"/>
+                                            </a>
+                                            <div className="card-bottom">
+                                                <div className="card-title">Drama</div>
+                                                <div className="card-subtitle">196 Shows, 1161 Movies</div>
+                                                <div className="btn btn-outline-coral d-desktop-inblock">Watch Now</div>
+                                            </div>
+                                        </div>
+                                        <div className="card-div lg-card mr-3">
+                                            <a href="#" className="card-lg-img img-top">
+                                                <img src="http://via.placeholder.com/280x250"/>
+                                            </a>
+                                            <div className="card-bottom">
+                                                <div className="card-title">Drama</div>
+                                                <div className="card-subtitle">196 Shows, 1161 Movies</div>
+                                                <div className="btn btn-outline-coral d-desktop-inblock">Watch Now</div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+
+                    {/*new channel list start*/}
+                    <div className="no-m-card-component card-component padd">
+                        <div className="card-wrapper">
+                            <div className="card-header mt-2">TV Channel Serial List <span className="card-view-all mr-2">View All</span></div>
+                            <div className="card-slider m-no-slider">
+                                <div className="card-div channel-card mr-2">
+                                    <div className="card-channel-logo img-center">
+                                        <img src="https://images.justwatch.com/icon/4233120/s100/hotstar" />
+                                    </div>
+                                    <div className="card-channel-text">
+                                        <div className="card-title">Star Plus</div>
+                                        <div className="card-subtitle">91 Shows</div>
+                                        <div className="btn btn-outline-coral d-desktop-inblock">View Details</div>
+                                    </div>
+                                </div>
+                                <div className="card-div channel-card mr-2">
+                                    <div className="card-channel-logo img-center">
+                                        <img src="https://images.justwatch.com/icon/4233120/s100/hotstar" />
+                                    </div>
+                                    <div className="card-channel-text">
+                                        <div className="card-title">Star Plus</div>
+                                        <div className="card-subtitle">91 Shows</div>
+                                        <div className="btn btn-outline-coral d-desktop-inblock">View Details</div>
+                                    </div>
+                                </div>
+                                <div className="card-div channel-card mr-2">
+                                    <div className="card-channel-logo img-center">
+                                        <img src="https://images.justwatch.com/icon/4233120/s100/hotstar" />
+                                    </div>
+                                    <div className="card-channel-text">
+                                        <div className="card-title">Star Plus</div>
+                                        <div className="card-subtitle">91 Shows</div>
+                                        <div className="btn btn-outline-coral d-desktop-inblock">View Details</div>
+                                    </div>
+                                </div>
+                                <div className="card-div channel-card mr-2">
+                                    <div className="card-channel-logo img-center">
+                                        <img src="https://images.justwatch.com/icon/4233120/s100/hotstar" />
+                                    </div>
+                                    <div className="card-channel-text">
+                                        <div className="card-title">Star Plus</div>
+                                        <div className="card-subtitle">91 Shows</div>
+                                        <div className="btn btn-outline-coral d-desktop-inblock">View Details</div>
+                                    </div>
+                                </div>
+                                <div className="card-div channel-card mr-2">
+                                    <div className="card-channel-logo img-center">
+                                        <img src="https://images.justwatch.com/icon/4233120/s100/hotstar" />
+                                    </div>
+                                    <div className="card-channel-text">
+                                        <div className="card-title">Star Plus</div>
+                                        <div className="card-subtitle">91 Shows</div>
+                                        <div className="btn btn-outline-coral d-desktop-inblock">View Details</div>
+                                    </div>
+                                </div>
+                                <div className="card-div channel-card mr-2">
+                                    <div className="card-channel-logo img-center">
+                                        <img src="https://images.justwatch.com/icon/4233120/s100/hotstar" />
+                                    </div>
+                                    <div className="card-channel-text">
+                                        <div className="card-title">Star Plus</div>
+                                        <div className="card-subtitle">91 Shows</div>
+                                        <div className="btn btn-outline-coral d-desktop-inblock">View Details</div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    {/*new channel list end*/}
                             {/*channel list start */}
 
-                    <div className="card-header mt-2">TV Channel Serial List <span className="card-view-all">View All</span></div>
+                    {/*<div className="card-header mt-2">TV Channel Serial List <span className="card-view-all">View All</span></div>
                     <div className="channel-wrapper">
                         <div className="row">
                             <div className="col-6 col-md-3">
@@ -93,6 +230,7 @@ class Home extends Component {
                                     <div className="card-channel-text">
                                         <div className="card-title">Star Plus</div>
                                         <div className="card-subtitle">91 Shows</div>
+                                        <div className="btn btn-outline-coral d-desktop-inblock">View Details</div>
                                     </div>
                                 </div>
                             </div>
@@ -130,10 +268,11 @@ class Home extends Component {
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div>*/}
                             {/*channel list end*/}
 
                     {/*new widget*/}
+{/*
                     <div className="row new1">
                         <div className="col-md-3 col-sm-4 col-6">
                             <div className="card-div lg-card mr-3">
@@ -180,6 +319,7 @@ class Home extends Component {
                             </div>
                         </div>
                     </div>
+*/}
                     {/*new widget end */}
                 </div>
             </div>
