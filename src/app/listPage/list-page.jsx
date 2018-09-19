d40e522119147c4a61427241a15847c2c1bc9fa5
91shreya <shreya@91mobiles.com>
2018-09-19 16:07:38 +0530

    pages

--- src/app/listPage/list-page.jsx
+++ src/app/listPage/list-page.jsx
@@ -15,12 +15,204 @@ class ListPage extends Component {
                <div className="container">
                    <div className="no-m-card-component card-component">
                    <div className="row no-gutters">
-                       <div className="col-12 col-sm-12 col-lg-3 left-snippet border-right d-desktop-block">
-                           <div className="padd">Filters</div>
+                       <div className="col-cs-12 col-cssm-12 col-cslg-3 border-right d-desktop-block">
+                           <div className="left-snippet">
+                               <div className="row filtertop">
+                                   <div className="col-6">Filters</div>
+                                   <div className="col-6 clr-all">Clear All</div>
+                               </div>
+                               <div className="fltrwrap">
+                                   <h5 className="fltrheading">MOVIE/TV SHOWS</h5>
+                                   <div className="d-filtrlist">
+                                   <ul>
+                                       <li>
+                                           <label className="filtrinput">
+                                           <span className="filterinputtext">Movies</span>
+                                           <input name="group1" type="radio" />
+                                           <span className="radiobtn"></span>
+                                       </label>
+                                       </li>
+                                       <li>
+                                           <label className="filtrinput">
+                                               <span className="filterinputtext">TV Shows</span>
+                                               <input name="group1" type="radio" />
+                                               <span className="radiobtn"></span>
+                                           </label>
+                                       </li>
+                                       <li className="active">
+                                           <label className="filtrinput">
+                                               <span className="filterinputtext">All</span>
+                                               <input name="group1" type="radio" checked />
+                                               <span className="radiobtn"></span>
+                                           </label>
+                                       </li>
+                                   </ul>
+                                   </div>
+                               </div>
+                               <div className="fltrwrap">
+                                   <h5 className="fltrheading">IMDB Rating</h5>
+                                   <div className="d-filtrlist">
+                                       <ul>
+                                           <li>
+                                               <label className="filtrinput">
+                                                   <span className="filterinputtext">1+</span>
+                                                   <input name="group2" type="radio" />
+                                                   <span className="radiobtn"></span>
+                                               </label>
+                                           </li>
+                                           <li>
+                                               <label className="filtrinput">
+                                                   <span className="filterinputtext">2+</span>
+                                                   <input name="group2" type="radio" />
+                                                   <span className="radiobtn"></span>
+                                               </label>
+                                           </li>
+                                           <li>
+                                               <label className="filtrinput">
+                                                   <span className="filterinputtext">3+</span>
+                                                   <input name="group2" type="radio" />
+                                                   <span className="radiobtn"></span>
+                                               </label>
+                                           </li>
+                                           <li>
+                                               <label className="filtrinput">
+                                                   <span className="filterinputtext">4+</span>
+                                                   <input name="group2" type="radio" />
+                                                   <span className="radiobtn"></span>
+                                               </label>
+                                           </li>
+                                           <li>
+                                               <label className="filtrinput">
+                                                   <span className="filterinputtext">5+</span>
+                                                   <input name="group2" type="radio" />
+                                                   <span className="radiobtn"></span>
+                                               </label>
+                                           </li>
+                                           <li>
+                                               <label className="filtrinput">
+                                                   <span className="filterinputtext">6+</span>
+                                                   <input name="group2" type="radio" />
+                                                   <span className="radiobtn"></span>
+                                               </label>
+                                           </li>
+                                           <li>
+                                               <label className="filtrinput">
+                                                   <span className="filterinputtext">7+</span>
+                                                   <input name="group2" type="radio"  />
+                                                   <span className="radiobtn"></span>
+                                               </label>
+                                           </li>
+                                           <li>
+                                               <label className="filtrinput">
+                                                   <span className="filterinputtext">8+</span>
+                                                   <input name="group2" type="radio" />
+                                                   <span className="radiobtn"></span>
+                                               </label>
+                                           </li>
+                                           <li>
+                                               <label className="filtrinput">
+                                                   <span className="filterinputtext">9+</span>
+                                                   <input name="group2" type="radio" />
+                                                   <span className="radiobtn"></span>
+                                               </label>
+                                           </li>
+                                       </ul>
+                                   </div>
+                               </div>
+                               <div className="fltrwrap">
+                                   <h5 className="fltrheading">LAUNCH DATE</h5>
+                                   <div className="d-filtrlist">
+                                       <ul>
+                                           <li>
+                                               <label className="filtrinput">
+                                                   <span className="filterinputtext">This Year (2018)</span>
+                                                   <input name="group3" type="radio" />
+                                                   <span className="radiobtn"></span>
+                                               </label>
+                                           </li>
+                                           <li>
+                                               <label className="filtrinput">
+                                                   <span className="filterinputtext">Last Year (2017)</span>
+                                                   <input name="group3" type="radio" />
+                                                   <span className="radiobtn"></span>
+                                               </label>
+                                           </li>
+                                           <li >
+                                               <label className="filtrinput">
+                                                   <span className="filterinputtext">Between 2 dates</span>
+                                                   <input name="group3" type="radio" checked />
+                                                   <span className="radiobtn"></span>
+                                               </label>
+                                               <div className="d-yr-range-wrp">
+                                                   <div className="d-yr-range-div">From {/*<i className="smdwnarw"></i>*/}</div>
+                                                   <span className="date-spacer">-</span>
+                                                   <div className="d-yr-range-div">To {/*<i className="smdwnarw"></i>*/}</div>
+                                               </div>
+                                           </li>
+                                       </ul>
+                                   </div>
+                               </div>
+                               <div className="fltrwrap">
+                                   <h5 className="fltrheading">Language</h5>
+                                   <div className="d-filtrlist">
+                                       <ul>
+                                           <li>
+                                               <label className="filtrinput">
+                                                   <span className="filterinputtext">Hindi (45) </span>
+                                                   <input type="checkbox" />
+                                                   <span className="checkmark"></span>
+                                               </label>
+                                           </li>
+                                           <li>
+                                               <label className="filtrinput">
+                                                   <span className="filterinputtext">Engish (15) </span>
+                                                   <input type="checkbox" />
+                                                   <span className="checkmark"></span>
+                                               </label>
+                                           </li>
+                                           <li>
+                                               <label className="filtrinput">
+                                                   <span className="filterinputtext">Tamil (10) </span>
+                                                   <input type="checkbox" />
+                                                   <span className="checkmark"></span>
+                                               </label>
+                                           </li>
+                                       </ul>
+                                   </div>
+                               </div>
+                               <div className="fltrwrap">
+                                   <h5 className="fltrheading">Genres</h5>
+                                   <div className="d-filtrlist">
+                                       <ul>
+                                           <li>
+                                               <label className="filtrinput">
+                                                   <span className="filterinputtext">Comedy (45) </span>
+                                                   <input type="checkbox" />
+                                                   <span className="checkmark"></span>
+                                               </label>
+                                           </li>
+                                           <li>
+                                               <label className="filtrinput">
+                                                   <span className="filterinputtext">Thriller (15) </span>
+                                                   <input type="checkbox" />
+                                                   <span className="checkmark"></span>
+                                               </label>
+                                           </li>
+                                           <li>
+                                               <label className="filtrinput disable">
+                                                   <span className="filterinputtext">Action & Adventure (45) </span>
+                                                   <input type="checkbox" />
+                                                   <span className="checkmark"></span>
+                                               </label>
+                                           </li>
+                                       </ul>
+                                   </div>
+                               </div>
+                           </div>
                        </div>
-                       <div className="col-12 col-sm-12 col-lg-9">
-                           <div className="padd ryt-snippet">
-                                   <div className="card-header mt-2">Popular Hindi TV Shows</div>
+                       <div className="col-cs-12 col-cssm-12 col-cslg-9">
+                           <div className="ryt-snippet">
+                                   <div className="card-header mt-2">580 RESULTS FOUND </div>
                                <div className="clearfix"></div>
                                <div className="listwrp">
                                    <div className="listcard">
